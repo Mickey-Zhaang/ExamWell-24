7544c808ac3c1d709fc93134c8365335d6e030ee
Mickey_zhaang <qzhang27@colby.edu>
2024-07-16 19:02:17 -0400

    Skeleton-Made, waiting for improvements... please

--- frontend/src/Routes/index.jsx
+++ frontend/src/Routes/index.jsx
@@ -1,6 +1,7 @@
 import { createBrowserRouter } from "react-router-dom";
 import Home from "../Pages/Home";
 
+
 export const router = createBrowserRouter([
   {
     path: "home",
--- frontend/src/components/HomeComponent.jsx
+++ frontend/src/components/HomeComponent.jsx
@@ -1,9 +1,18 @@
-import React, { useState } from "react";
+import React from "react";
+import "../Sass/HomeComponent.scss";
 
-export default function HomeComponent() {
+const HomeComponent = () => {
   return (
-    <div>
-      <h1>Hello World</h1>
+    <div className="home-wrapper">
+      <div className="home">
+        <h1 className="home__title">ExamWell</h1>
+        <p className="home__statement"><i>"Whether you're a teacher that is looking to facilitate the test creation process or a student that is looking to study better, ExamWell is the go-to tool for efficient education."</i></p>
+      </div>
+      <div>
+        <input className="search_bar" type="text" placeholder="What are you looking for?"/>
+      </div>
     </div>
   );
-}
+};
+
+export default HomeComponent;
